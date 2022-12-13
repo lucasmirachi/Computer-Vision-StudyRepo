b3bc0e8807c6239431883eb46ca5c029e3472bbd
Lucas Mirachi <lucas.mirachi@gmail.com>
2022-12-13 19:54:33 -0300

    Update read.py

--- exercises/2A-L1 Images as functions/read.py
+++ exercises/2A-L1 Images as functions/read.py
@@ -1,5 +1,10 @@
 import numpy as np
 import cv2
 
-img = cv2.imread("test.jpeg")
-cv2.imshow(img)
+img = cv2.imread('test.jpeg', 0)
+
+print(img)
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
